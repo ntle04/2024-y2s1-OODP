2db460729b5d31927772a6cccce6a16f1ce337e8
ntle04 <natalieng.99@gmail.com>
2024-09-16 11:38:53 +0800

    code for VendingMachine class and application class VendingMachineApp

--- Tutorial3/VendingMachineApp/VendingMachine.java
+++ Tutorial3/VendingMachineApp/VendingMachine.java
@@ -1,5 +1,70 @@
 package Tutorial3.VendingMachineApp;
+import java.util.Scanner;
 
 public class VendingMachine{
-    
+    //constructor 
+    public VendingMachine(){    }
+    Scanner sc = new Scanner(System.in);
+
+    //get drink selection and return cost of drink
+    public double selectDrink(){
+        System.out.println("Please enter selection:");
+        int choice = sc.nextInt();
+        switch(choice){
+            case 1:
+                return 3.00;
+            case 2:
+                return 1.00;
+            case 3:
+                return 5.00;
+            default:
+                System.out.println("Invalid selection!");
+        }
+        return 0.00;
+    }
+
+    //insert coins and return amount inserted
+    public double insertCoins(double drinkCost){
+        double amount = 0.00;
+        do{
+            String coin = "";
+            coin = sc.next();
+            switch(coin){
+                case "Q":
+                case "q":
+                    amount += 0.10;
+                    break;
+                case "T":
+                case "t":
+                    amount += 0.20;
+                    break;
+                case "F":
+                case "f":
+                    amount += 0.50;
+                    break;
+                case "N":
+                case"n":
+                    amount += 1.00;
+                    break;
+                default:
+                    System.out.println("Enter a valid coin selection!");
+            }
+            System.out.printf("Coins inserted: %.2f\n", amount);
+        } while(amount < drinkCost);
+        return amount;
+    }
+
+    //check change and print change on screen
+    public void checkChange(double amount, double drinkCost){
+        if(amount >= drinkCost){
+            System.out.printf("Change: $ %.2f\n", (amount - drinkCost));
+        }
+        return;
+    }
+
+    //print receipt and collect drink
+    public void printReceipt(){
+        System.out.println("Please collect your drink");
+        System.out.println("Thank you !!");
+    }
 }
--- Tutorial3/VendingMachineApp/VendingMachineApp.java
+++ Tutorial3/VendingMachineApp/VendingMachineApp.java
@@ -1,5 +1,34 @@
 package Tutorial3.VendingMachineApp;
 
 public class VendingMachineApp {
-    
+    public static void main(String[] args){
+        VendingMachine vm = null;
+
+        printMenu();
+        vm = new VendingMachine();
+        double toPay = vm.selectDrink();
+        printCoinsInput();
+        double paid = vm.insertCoins(toPay);
+        vm.checkChange(paid, toPay);
+        vm.printReceipt();
+    }
+        
+
+    public static void printMenu(){
+        System.out.println("====== Vending Machine ======");
+        System.out.println("|1. Buy Beer ($3.00)        |");
+        System.out.println("|2. Buy Coke ($1.00)        |");
+        System.out.println("|3. Buy Green Tea ($5.00)   |");
+        System.out.println("=============================");
+    }
+
+    public static void printCoinsInput(){
+        System.out.println("Please insert coins: ");
+        System.out.println("========== Coins Input ===========");
+        System.out.println("|Enter 'Q' for ten cents input   |");
+        System.out.println("|Enter 'T' for twenty cents input|");
+        System.out.println("|Enter 'F' for fifty cents input |");
+        System.out.println("|Enter 'N' for a dollar input    |");
+        System.out.println("==================================");
+    }
 }
